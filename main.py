8e8995e26cf09ccbb5bad5d69b2f9d54170cd1c5
Rigaffe <34256332+Rigaffe@users.noreply.github.com>
2021-01-22 15:55:53 +0100

    11

--- main.py
+++ main.py
@@ -1,3 +1,30 @@
 import time
 import random
 import sys
+
+
+def title_screen_menu():
+    print("################")
+    print("##  Welcome  ###")
+    print("################")
+    print("#              #")
+    print("#   - play -   #")
+    print("#   - quit -   #")
+    print("#              #")
+    print("################")
+
+    # Main menu
+    options = True
+    while options != "play" and options != "quit":
+        options = input("> ")
+
+    if options == "play":
+        print("Start game ...")
+        pass
+    elif options == "quit":
+        print("Closing ...")
+        sys.exit()
+
+
+if __name__ == "__main__":
+    title_screen_menu()
